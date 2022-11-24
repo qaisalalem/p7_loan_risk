b8d2ed4a4fbaf6cbce0de4c91107e7799ebd1966
qaisalalem <36120018+qaisalalem@users.noreply.github.com>
2022-11-24 01:19:16 +0100

    Update dashboard3.py

--- dashboard3.py
+++ dashboard3.py
@@ -39,21 +39,23 @@ def load_data(file):
 
 def preprocessing(data, num_imputer, bin_imputer, transformer, scaler):
     """This function is used to perform data preprocessing."""
-    X_df = data.drop(['SK_ID_CURR'], axis=1)
+    
 
     # Feature selection
     # Categorical features
+    
+    X_df = data.drop(['SK_ID_CURR'], axis=1)
+
+    # Feature selection
+    numeric_features = list(X_df.select_dtypes('int64').nunique().index)
+    numeric_features.extend(list(X_df.select_dtypes('float64').nunique().index))
     cat_features = list(X_df.select_dtypes('object').nunique().index)
 
     # Encoding categorical features
-    df = pd.get_dummies(X_df, columns=cat_features)
-
-    # Numerical and binary features
-    features_df = df.nunique()
-    num_features = list(features_df[features_df != 2].index)
-    binary_features = list(features_df[features_df == 2].index)
-    #df['NAME_FAMILY_STATUS_Unknown'] = 0
-    #binary_features.append('NAME_FAMILY_STATUS_Unknown')
+    X = pd.get_dummies(X_df, columns=cat_features)
+    df = X.nunique()
+    binary_features = list(df[df == 2].index)
+    num_features = list(df[df != 2].index)
 
     # Imputations
     X_num = pd.DataFrame(num_imputer.transform(df[num_features]),
