5bda47fb28c585213e8ef93718ce989ad94b27ed
qaisalalem <36120018+qaisalalem@users.noreply.github.com>
2022-11-24 02:41:18 +0100

    Update dashboard3.py

--- dashboard3.py
+++ dashboard3.py
@@ -58,6 +58,7 @@ def preprocessing(data, num_imputer, bin_imputer, transformer, scaler):
     #df['NAME_FAMILY_STATUS_Unknown'] = 0
     #binary_features.append('NAME_FAMILY_STATUS_Unknown')
     st.write(len(binary_features))
+    st.write(binary_features)
 
     # Imputations
     X_num = pd.DataFrame(num_imputer.transform(df[num_features]),
