cae8aaeaf35fa67a9b29c0b8cd8fc21201212d2f
qaisalalem <36120018+qaisalalem@users.noreply.github.com>
2022-11-28 18:49:54 +0100

    Update dashboard3.py

--- dashboard3.py
+++ dashboard3.py
@@ -211,7 +211,7 @@ def main():
     st.subheader('Graph showing variable 1')
     a=sns.distplot(dash[variable1], bins=30)
     a.axvline(x=amt_inc_total)
-    a.text(amt_inc_total,variable1.max(),'selected client',rotation=90)
+    a.text(amt_inc_total,verticalalignment='top','selected client',rotation=90)
     st.pyplot()
 
     #st.pyplot(fig)
