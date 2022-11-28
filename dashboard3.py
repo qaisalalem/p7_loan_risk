b6e609a624fad3868f8671d2f46ffe6517a3f1f4
qaisalalem <36120018+qaisalalem@users.noreply.github.com>
2022-11-28 19:30:24 +0100

    Update dashboard3.py

--- dashboard3.py
+++ dashboard3.py
@@ -167,6 +167,7 @@ def main():
                         'ANNUITY_INCOME_RATE','INCOME_CREDIT_RATE','DAYS_BIRTH',
                         'REGION_POPULATION_RELATIVE']
     X = norm_df[norm_df.SK_ID_CURR == customer_id]
+    st.write(X)
     X = X.drop(['SK_ID_CURR'], axis=1)
     X= X[relevant_features]
     
